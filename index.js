3c24d262dc73e397c12c8ae7cde198c850c191a3
Leandro Santos <leandrocmsantos@gmail.com>
2023-04-17 00:29:18 -0300

    refactor code

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const SpotifyWebApi = require('spotify-web-api-node');
+const spotifyApi = require('./spotify');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configurações do Spotify
-const spotifyApi = new SpotifyWebApi({
-    clientId: '340fa9017d0c43b4a1e03b9e5790a948',
-    clientSecret: 'a9f3fb4ba6594143a999ed91c54aa291',
-});
 
-// Autentica o cliente e obtém um token de acesso válido
-spotifyApi.clientCredentialsGrant()
-    .then(data => {
-        spotifyApi.setAccessToken(data.body.access_token);
-        console.log('Cliente autenticado com sucesso!');
-    })
-    .catch(error => {
-        console.log('Erro ao autenticar o cliente:', error);
-    });
 // Configurações da API
 app.use(bodyParser.json());
 
